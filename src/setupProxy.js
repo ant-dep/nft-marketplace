d0537c85644c8eb1c2bb5e69a916fd85a785167c
Antoine P <depertat@hotmail.fr>
2021-12-15 14:16:30 +0100

    axios request updated. scale effect added to owner name

---
+++ src/setupProxy.js
@@ -0,0 +1,12 @@
+const { createProxyMiddleware } = require('http-proxy-middleware'​);
+
+
+module.exports = function(app) {
+  app.use(
+    '​/api'​,
+    createProxyMiddleware({
+      target: 'http://siteurl1.com'​,
+      changeOrigin: true,
+  })
+);
+};
